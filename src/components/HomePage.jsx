0b9e391d30840f8f511c8b4ee5f991027a9bb824
mojpm <itsjoelpm@gmail.com>
2021-02-26 17:28:03 +0300

    implement search form functionality

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,15 +2,46 @@ import React, { useEffect, useRef, useState } from "react";
 import "./HomePage.scss";
 import * as FaIcons from "react-icons/fa";
 import * as GrIcons from "react-icons/gr";
+import * as MdIcons from "react-icons/md";
 import testData from "../data/mockGithubJobs";
 import Spinner from "./Spinner";
+import JobListings from "./JobListings";
 
+/*
+  TODO 
+ * iii) Handle empty data returned 
+ * iv) Handle Page number for search terms 
+ * iii) Craete Page for job description
+ * iv) Remove Form Validation => Not Neccesary
+ * 
+ */
 export default function HomePage() {
+  const searchObject = {
+    keyword: "",
+    location: "",
+    jobtype: "",
+  };
+
+  const termsObject = {
+    keyword: "",
+    location: "",
+    jobtype: "",
+  };
+
+  // Default Page State
   const isMounted = useRef(false);
   const [jobListings, setJobListings] = useState(testData);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
   const [pageNumber, setPageNumber] = useState(1);
+
+  // Form State
+  const [searchJobListings, setSearchJobListings] = useState(null);
+  const [searchLoading, setSearchLoading] = useState(true);
+  const [searchError, setSearchError] = useState(null);
+  const [searchPageNumber, setSearchPageNumber] = useState(1);
+  const [searchTerms, setSearchTerms] = useState(searchObject);
+  const [selectedTerms, setSelectedTerms] = useState(termsObject);
 
   useEffect(() => {
     isMounted.current = true;
@@ -41,23 +72,75 @@ export default function HomePage() {
     };
   }, [pageNumber]);
 
-  const generateDate = (date) => {
-    const splitDate = date.split(" ");
-    return `${splitDate[0]}, ${splitDate[1]} ${splitDate[2]}`;
+  const handleChange = (event) => {
+    setSearchTerms((currentTerm) => {
+      return { ...currentTerm, [event.target.id]: event.target.value };
+    });
   };
 
-  const handlePrev = () => {
-    setPageNumber((pageNumber) => pageNumber - 1);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setSearchTerms(() => {
+      return { ...searchTerms, [event.target.id]: event.target.value };
+    });
+
+    findSearchJobs();
   };
 
-  const handleNext = () => {
-    setPageNumber((pageNumber) => pageNumber + 1);
+  const handleTerms = (event) => {
+    setSelectedTerms(() => {
+      return { ...selectedTerms, [event.target.id]: event.target.value };
+    });
   };
 
-  console.log(pageNumber);
+  const removeTag = (event) => {
+    setSelectedTerms(() => {
+      return {
+        ...selectedTerms,
+        [event.target.parentNode.id]: "",
+      };
+    });
+
+    findSearchJobs();
+  };
+
+  const findSearchJobs = () => {
+    const { keyword, location, jobtype } = selectedTerms;
+
+    return fetch(
+      `https://github-api-next.vercel.app/api/positions${
+        keyword ? "?description=" + keyword : "?search=code"
+      }${
+        jobtype
+          ? jobtype === "FullTime"
+            ? "&full_time=true"
+            : "&part_time=true"
+          : ""
+      }${location ? "&location=" + location : ""}`
+    )
+      .then((res) => {
+        console.log(res);
+        return res.json();
+      })
+      .then((jobs) => {
+        setSearchJobListings(jobs.data);
+      })
+      .catch((error) => {
+        console.error("There was an error => ", error);
+        setSearchError(error);
+      })
+      .finally(() => {
+        setSearchLoading(false);
+      });
+  };
+
+  useEffect(findSearchJobs, [selectedTerms]);
 
   if (error) throw error;
   if (loading) return <Spinner />;
+  if (searchError) throw searchError;
+
   return (
     <main>
       <header className="header">
@@ -77,71 +160,114 @@ export default function HomePage() {
         </div>
       </header>
 
-      <section className="search-bar">
-        <div>
-          <input type="input" placeholder="Keywords e.g java" />
-        </div>
-        <div>
-          <input type="input" placeholder="Location" />
-        </div>
-        <div>
-          <select name="type" id="job-type">
-            <option value="Job Type">Job Type</option>
+      <section className="search-form">
+        <form className="search-bar" onSubmit={handleSubmit}>
+          <input
+            type="input"
+            id="keyword"
+            placeholder="Keyword e.g java"
+            value={searchTerms.keyword}
+            onChange={handleChange}
+            onBlur={handleTerms}
+          />
+          <input
+            type="input"
+            placeholder="Location"
+            id="location"
+            value={searchTerms.location}
+            onChange={handleChange}
+            onBlur={handleTerms}
+          />
+          <select
+            name="type"
+            id="jobtype"
+            onChange={handleChange}
+            onBlur={handleTerms}
+          >
+            <option value="">Choose Job Type</option>
             <option value="FullTime">FullTime</option>
             <option value="PartTime">PartTime</option>
           </select>
+
+          <input type="submit" value="Search Jobs" />
+        </form>
+        <div className="term-tags">
+          {selectedTerms.keyword ? (
+            <div className="tag">
+              {selectedTerms.keyword}{" "}
+              <span onClick={removeTag} id="keyword">
+                <MdIcons.MdCancel />
+              </span>
+            </div>
+          ) : (
+            ""
+          )}
+
+          {selectedTerms.location ? (
+            <div className="tag">
+              {selectedTerms.location}{" "}
+              <span onClick={removeTag} id="location">
+                <MdIcons.MdCancel />
+              </span>
+            </div>
+          ) : (
+            ""
+          )}
+
+          {selectedTerms.jobtype ? (
+            <div className="tag">
+              {selectedTerms.jobtype}{" "}
+              <span onClick={removeTag} id="jobtype">
+                <MdIcons.MdCancel value="jobtype" />
+              </span>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
-        <div>
-          <button type="submit">Search Jobs</button>
-        </div>
-        <div></div>
       </section>
 
-      {jobListings.map((job) => (
-        <section className="job-listing" key={job.id}>
-          <div className="ad-info">
-            <div className="logo">
-              <img
-                src={job.company_logo}
-                alt="Company Logo"
-                className="company-logo"
-              />
-            </div>
-            <h2>{job.company}</h2>
-            <a href={job.company_url ? `${job.company_url}` : "#"}>
-              Visit Company Website
-            </a>
+      {searchJobListings ? (
+        <>
+          <JobListings jobListings={searchJobListings} />
+          <div>
+            <button
+              onClick={() => {
+                window.scrollTo(0, 0);
+              }}
+            >
+              Back To Top
+            </button>
           </div>
-          <div className="job-title">
-            <h1>{job.title}</h1>
-            <p>{generateDate(job.created_at)}</p>
-          </div>
-          <div className="tags">
-            <div className="cat">
-              <p>software dev</p>
+        </>
+      ) : (
+        <>
+          <JobListings jobListings={jobListings} />
+          <section className="nav-buttons">
+            <div>
+              <GrIcons.GrPrevious />
+              <button
+                disabled={pageNumber > 1 ? false : true}
+                onClick={() => {
+                  setPageNumber((pageNumber) => pageNumber - 1);
+                }}
+              >
+                Prev
+              </button>
             </div>
-            <div className="cat">
-              <p>{job.type}</p>
+            <div>
+              <button
+                onClick={() => {
+                  setPageNumber((pageNumber) => pageNumber + 1);
+                }}
+              >
+                Next
+              </button>
+              <GrIcons.GrNext />
             </div>
-            <div className="cat">
-              <p>{job.location}</p>
-            </div>
-          </div>
-        </section>
-      ))}
-
-      <section className="nav-buttons">
-        <div>
-          <GrIcons.GrPrevious />
-          <button disabled={pageNumber > 1 ? false : true} onClick={handlePrev}>
-            Prev
-          </button>
-        </div>
-        <div>
-          <button onClick={handleNext}>Next</button>
-          <GrIcons.GrNext />
-        </div>
-      </section>
+          </section>
+        </>
+      )}
     </main>
   );
 }
