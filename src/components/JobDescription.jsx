4387e0b61fd9c26b93623773d6beac3a2c8f318f
mojpm <itsjoelpm@gmail.com>
2021-03-01 00:13:16 +0300

    add job description page

---
+++ src/components/JobDescription.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function JobDescription({ jobid }) {
+  return (
+    <>
+      <div>
+        <h1> Welcome to the Job Description</h1>
+      </div>
+    </>
+  );
+}
