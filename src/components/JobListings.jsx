bef04a775854cebf27b25e7ed33773a499fc74c4
mojpm <itsjoelpm@gmail.com>
2021-03-04 08:20:08 +0300

    add job description

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,51 +1,43 @@
-import React, { useState } from "react";
+import React from "react";
 import { format } from "date-fns";
-import JobDescription from "./JobDescription";
-export default function JobListings({ jobListings }) {
-  const [jobId, setJobId] = useState(null); 
+import { Link } from "react-router-dom";
 
-  /*
-   * Going to implement Job Description Page Tomorrow => Too tired :(
-   */
+export default function JobListings({ jobListings }) {
   return (
     <>
       {jobListings.map((job) => (
-        <section
-          className="job-listing"
-          key={job.id}
-          onClick={() => {
-            setJobId(job.id);
-          }}
-        >
-          <div className="ad-info">
-            <div className="logo">
-              <img
-                src={job.company_logo}
-                alt="Company Logo"
-                className="company-logo"
-              />
+        <Link to={`/${job.id}`}>
+          <section className="job-listing" key={job.id}>
+            <div className="ad-info">
+              <div className="logo">
+                <img
+                  src={job.company_logo}
+                  alt="Company Logo"
+                  className="company-logo"
+                />
+              </div>
+              <h2>{job.company}</h2>
+              <a href={job.company_url ? `${job.company_url}` : "#"}>
+                Visit Company Website
+              </a>
             </div>
-            <h2>{job.company}</h2>
-            <a href={job.company_url ? `${job.company_url}` : "#"}>
-              Visit Company Website
-            </a>
-          </div>
-          <div className="job-title">
-            <h1>{job.title}</h1>
-            <p>{format(new Date(job.created_at), "E, MMM io")}</p>
-          </div>
-          <div className="tags">
-            <div className="cat">
-              <p>software dev</p>
+            <div className="job-title">
+              <h1>{job.title}</h1>
+              <p>{format(new Date(job.created_at), "E, MMM io")}</p>
             </div>
-            <div className="cat">
-              <p>{job.type}</p>
+            <div className="tags">
+              <div className="cat">
+                <p>software dev</p>
+              </div>
+              <div className="cat">
+                <p>{job.type}</p>
+              </div>
+              <div className="cat">
+                <p>{job.location}</p>
+              </div>
             </div>
-            <div className="cat">
-              <p>{job.location}</p>
-            </div>
-          </div>
-        </section>
+          </section>
+        </Link>
       ))}
     </>
   );
