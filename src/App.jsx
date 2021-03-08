50f41d7d4b173369f61a3c19719a92bc069d5bec
mojpm <itsjoelpm@gmail.com>
2021-03-08 23:13:42 +0300

    modify import paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./components/HomePage";
-import JobDescription from "./components/JobDescription";
-import "./App.scss";
+import HomePage from "./components/HomePage/HomePage";
+import JobDescription from "./components/JobDescription/JobDescription";
+import { createGlobalStyle } from "styled-components";
+import { COLORS } from "./styles/rootStyles";
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: ${COLORS.white};
+  }
+  
+`;
 
 export default function App() {
   return (
     <>
+      <GlobalStyles />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/:jobId" element={<JobDescription />} />
